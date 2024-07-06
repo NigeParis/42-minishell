521d33a95a66305f8297f21b6e88b0446069e8c0
nrobinso <nrobinso@student.42.fr>
2024-07-06 10:23:23 +0200

    my solution to echo no crashes but not NORM

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 14:43:26 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/07/05 15:28:53 by bgoulard         ###   ########.fr       */
+/*   Updated: 2024/07/06 10:21:39 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,57 @@ void	handl_opt(char *arg, bool *flag_n, bool *error)
 	while (arg[i])
 	{
 		if (arg[i] == 'n')
+		{
 			*flag_n = true;
-		else
 			*error = false;
+		}
+		else
+		{
+			*flag_n = false;
+			*error = true;
+			break ;
+		}
 		i++;
 	}
 	return ;
 }
+#include <stdio.h>
 
 int	echo_main(t_minishell_control *ctrl, t_cmd_to_exec *cmd)
 {
 	int		i;
+	int		j;
 	bool	flag_n;
 	bool	error;
 
 	i = 1;
+	j = 1;
 	flag_n = false;
 	error = false;
-	while (cmd->argv[i][0] == '-' && cmd->argv[i][1] != '-')
-		handl_opt(cmd->argv[i++], &flag_n, &error);
-	while (i < cmd->ac)
+	if (cmd->ac > 1)
 	{
-		add_to_buff(cmd->argv[i++], STDOUT_FILENO);
-		if (!flag_n)
-			add_to_buff("\n", STDOUT_FILENO);
+		while (cmd->argv[i] && cmd->argv[i][0] == '-' && cmd->argv[i][1] != '-')
+		{
+			handl_opt(cmd->argv[i++], &flag_n, &error);
+			if (error == true)
+				j == i - 1;
+			else
+				j++;
+		}
+		i = j;
+		while (i < cmd->ac)
+		{
+			add_to_buff(cmd->argv[i++], STDOUT_FILENO);
+			if (i != cmd->ac)
+				add_to_buff(" ", STDOUT_FILENO);
+			if (i == cmd->ac)
+			{
+				if (cmd->argv[1][0] == '-' && cmd->argv[1][1] != '-')
+					handl_opt(cmd->argv[1], &flag_n, &error);
+				if ((!flag_n && error) || (!flag_n && !error))
+					add_to_buff("\n", STDOUT_FILENO);
+			}
+		}
 	}
 	if (i == 1 && !flag_n)
 			add_to_buff("\n", STDOUT_FILENO);
